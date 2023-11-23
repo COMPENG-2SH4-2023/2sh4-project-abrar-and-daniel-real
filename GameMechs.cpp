4ec3e543735e74834439820e5db12fecf9038a04
unknown <tamjida@mcmaster.ca>
2023-11-22 20:24:05 -0500

    game mech gg

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -1,13 +1,24 @@
 #include "GameMechs.h"
+#include <iostream>
 
 GameMechs::GameMechs()
 {
-
+    input = '\0';
+    exitFlag = false;
+    loseFlag = false;
+    score = 0;
+    boardSizeX = 20;
+    boardSizeY = 10;
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-
+    input = '\0'; 
+    exitFlag = false;
+    loseFlag = false; 
+    score = 0;
+    boardSizeX = boardX;
+    boardSizeY = boardY;
 }
 
 // do you need a destructor?
@@ -16,38 +27,54 @@ GameMechs::GameMechs(int boardX, int boardY)
 
 bool GameMechs::getExitFlagStatus()
 {
-
+    return exitFlag;
 }
+
+bool GameMechs::getLoseFlagStatus()
+{
+    return loseFlag;
+}
+
+
 
 char GameMechs::getInput()
 {
-
+    return input;
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
+    exitFlag = true;
+}
 
+void GameMechs::setLoseFlag()
+{
+    loseFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
 void GameMechs::clearInput()
 {
-
+    input = '\0';
 }
 
 
+void GameMechs::incrementScore()
+{
+    score++;
+}
