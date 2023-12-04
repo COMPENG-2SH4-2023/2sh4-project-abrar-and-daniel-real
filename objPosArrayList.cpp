d48c7e7e24d933d0abd41cb59a9f00aa0c9a9528
unknown <tamjida@mcmaster.ca>
2023-12-03 22:34:04 -0500

    fjhsdlkfh;aosdfh

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -9,7 +9,7 @@ objPosArrayList::objPosArrayList()
     sizeList = 0;
     sizeArray = ARRAY_MAX_CAP;
 }
-
+//destructor
 objPosArrayList::~objPosArrayList()
 {
     delete[] aList;
@@ -24,13 +24,14 @@ void objPosArrayList::insertHead(objPos thisPos)
 {
     if (sizeList == sizeArray)
     {
-    
+            return; //do nothing if the list is full
     }
     for (int i = sizeList; i > 0; i--)
     {
+        //move indexes down by
         aList[i].setObjPos(aList[i - 1]);
     }
-    aList[0].setObjPos(thisPos);
+    aList[0].setObjPos(thisPos);   //insert @ sign at the start
     sizeList++;
 }
 
@@ -38,24 +39,27 @@ void objPosArrayList::insertTail(objPos thisPos)
 {
     if (sizeList == sizeArray)
     {
-        
+        return;  //do nothing if full
     }
+    //add to end of the list and increase the size by one
     aList[sizeList].setObjPos(thisPos);
     sizeList++;
 }
 
 void objPosArrayList::removeHead()
 {
-    
+    //shift all elements up an index
     for (int i = 0; i < sizeList; i++)
     {
         aList[i].setObjPos(aList[i + 1]);
     }
+    //decrease the size of the list
     sizeList--;
 }
 
 void objPosArrayList::removeTail()
 {
+    //remove last element and delete last index
     aList[sizeList - 1].setObjPos(aList[sizeList]);
     sizeList --;
 }
